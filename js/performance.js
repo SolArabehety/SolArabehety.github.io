aebad9554d21e6d66e21a2cb7a69e2fa5389e211
SolArabehety <sol.arabehety@gmail.com>
2023-03-07 15:49:01 -0300

    added stringify

--- js/performance.js
+++ js/performance.js
@@ -8,29 +8,29 @@
 
   new PerformanceObserver(entryList => {
     var response = entryList.getEntries()[0];
-    console.log('first-input', response);
+    console.log('first-input', JSON.stringify(response));
 
     if (window.webkit) {
-      window.webkit.messageHandlers.embraceFID.postMessage(response);
+      window.webkit.messageHandlers.embraceFID.postMessage(JSON.stringify(response));
     }
   }).observe({type: 'first-input', buffered: true});
 
   new PerformanceObserver(entryList => {
     var response = entryList.getEntriesByName('first-contentful-paint');
-    console.log('first-contentful-paint', response);
+    console.log('first-contentful-paint', JSON.stringify(response));
 
     if (window.webkit) {
-      window.webkit.messageHandlers.embraceFCP.postMessage(response);
+      window.webkit.messageHandlers.embraceFCP.postMessage(JSON.stringify(response));
     }
   }).observe({type: 'paint', buffered: true});
 
   new PerformanceObserver(entryList => {
     var response = entryList.getEntries();
 
-    console.log('largest-contentful-paint', response);
+    console.log('largest-contentful-paint', JSON.stringify(response));
 
     if (window.webkit) {
-      window.webkit.messageHandlers.embraceLCP.postMessage(response);
+      window.webkit.messageHandlers.embraceLCP.postMessage(JSON.stringify(response));
     }
   }).observe({type: 'largest-contentful-paint', buffered: true});
 })();
