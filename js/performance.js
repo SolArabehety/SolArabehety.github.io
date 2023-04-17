c224872d08fffe22cb904d624b4f4979ad8ea68a
SolArabehety <sol.arabehety@gmail.com>
2023-04-17 16:33:22 -0300

    script

--- js/performance.js
+++ js/performance.js
@@ -1,15 +1,55 @@
-'use strict';
+"use strict";
 
+const PLATFORMS = {
+  RN: "REACT_NATIVE",
+  A: "Android",
+  I: "iPhone",
+  D: "Default",
+};
+
+const POST_MESSAGE_BY_PLATFORM = {
+  REACT_NATIVE: window.ReactNativeWebView
+    ? window.ReactNativeWebView.postMessage
+    : console.log,
+  Android: (message) => console.log("EMBRACE_METRIC", message),
+  iOS: window.webkit
+    ? window.webkit.messageHandlers?.embrace?.postMessage
+    : console.log,
+  Default: console.log,
+};
+
+function getPlatform() {
+  const isRN = !!window.ReactNativeWebView;
+  if (isRN) return PLATFORMS.RN;
+  const isAndroid = window.navigator.userAgent.includes(PLATFORMS.A);
+  if (isAndroid) return PLATFORMS.A;
+  const isIos =
+    window.webkit && window.navigator.userAgent.includes(PLATFORMS.I);
+  if (isIos) return PLATFORMS.I;
+  return PLATFORMS.D;
+}
+
+const platform = getPlatform();
 /**
  * POC Webview support
  */
-(function() {
-  console.log('SDKs Webview support - POC');
+(function () {
+  console.log("SDKs Webview support - POC");
   handleCalculateFID();
   handleCalculateFCP();
   handleCalculateLCP();
   handleCalculateCLS();
 })();
+
+/**
+ * @param {
+ *  message: string;
+ * } message
+ */
+function sendMessageToEmbrace(message) {
+  const messageToSend = coreVitalParser(message);
+  POST_MESSAGE_BY_PLATFORM[platform](messageToSend);
+}
 
 /**
  * @param {{
@@ -22,42 +62,40 @@
 function coreVitalParser(vitals) {
   if (!Array.isArray(vitals)) {
     return console.log("`vitals` is not an array. It's not possible to parse");
-  };
+  }
 
   const METRICS = {
-    'largest-contentful-paint': 'LCP',
-    'paint': 'FCP',
-    'layout-shift': 'CLS',
-    'first-input': 'FID',
+    "largest-contentful-paint": "LCP",
+    paint: "FCP",
+    "layout-shift": "CLS",
+    "first-input": "FID",
   };
 
-  const response = JSON.parse(JSON.stringify(vitals)).map(({ name, entryType, startTime, duration, ...otherCoreVitals }) => {
-    return ({
-      n: !!name ? name : entryType,
-      t: METRICS[entryType],
-      st: startTime,
-      d: duration,
-      u: window.location.href,
-      p: otherCoreVitals
-    })
-  });
+  const response = JSON.parse(JSON.stringify(vitals)).map(
+    ({ name, entryType, startTime, duration, ...otherCoreVitals }) => {
+      return {
+        n: !!name ? name : entryType,
+        t: METRICS[entryType],
+        st: startTime,
+        d: duration,
+        u: window.location.href,
+        p: otherCoreVitals,
+      };
+    }
+  );
 
   return JSON.stringify(response);
-};
+}
 
 /**
  * FID
  * https://web.dev/fid/#measure-fid-in-javascript
  */
 function handleCalculateFID() {
-  new PerformanceObserver(entryList => {
+  new PerformanceObserver((entryList) => {
     var fidEntries = entryList.getEntries();
-    console.log('EMBRACE_METRIC', coreVitalParser(fidEntries));
-
-    if (window.webkit) {
-      window.webkit.messageHandlers.embrace.postMessage(coreVitalParser(fidEntries));
-    }
-  }).observe({ type: 'first-input', buffered: true });
+    sendMessageToEmbrace(fidEntries);
+  }).observe({ type: "first-input", buffered: true });
 }
 
 /**
@@ -65,14 +103,10 @@ function handleCalculateFID() {
  * https://web.dev/fcp/#measure-fcp-in-javascript
  */
 function handleCalculateFCP() {
-  new PerformanceObserver(entryList => {
-    var fcpEntries = entryList.getEntriesByName('first-contentful-paint');
-    console.log('EMBRACE_METRIC', coreVitalParser(fcpEntries));
-
-    if (window.webkit) {
-      window.webkit.messageHandlers.embrace.postMessage(coreVitalParser(fcpEntries));
-    }
-  }).observe({ type: 'paint', buffered: true });
+  new PerformanceObserver((entryList) => {
+    var fcpEntries = entryList.getEntriesByName("first-contentful-paint");
+    sendMessageToEmbrace(fcpEntries);
+  }).observe({ type: "paint", buffered: true });
 }
 
 /**
@@ -80,15 +114,10 @@ function handleCalculateFCP() {
  * https://web.dev/lcp/#measure-lcp-in-javascript
  */
 function handleCalculateLCP() {
-  new PerformanceObserver(entryList => {
+  new PerformanceObserver((entryList) => {
     var lcpEntries = entryList.getEntries();
-
-    console.log('EMBRACE_METRIC', coreVitalParser(lcpEntries));
-
-    if (window.webkit) {
-      window.webkit.messageHandlers.embrace.postMessage(coreVitalParser(lcpEntries));
-    }
-  }).observe({ type: 'largest-contentful-paint', buffered: true });
+    sendMessageToEmbrace(lcpEntries);
+  }).observe({ type: "largest-contentful-paint", buffered: true });
 }
 
 /**
@@ -116,9 +145,11 @@ function handleCalculateCLS() {
       // If the entry occurred less than 1 second after the previous entry and
       // less than 5 seconds after the first entry in the session, include the
       // entry in the current session. Otherwise, start a new session.
-      if (sessionValue &&
+      if (
+        sessionValue &&
         entry.startTime - lastSessionEntry.startTime < 1000 &&
-        entry.startTime - firstSessionEntry.startTime < 5000) {
+        entry.startTime - firstSessionEntry.startTime < 5000
+      ) {
         sessionValue += entry.value;
         sessionEntries.push(entry);
       } else {
@@ -131,14 +162,8 @@ function handleCalculateCLS() {
       if (sessionValue > clsValue) {
         clsValue = sessionValue;
         clsEntries = sessionEntries;
-
-        // Log the updated value (and its entries) to the console.
-        console.log('EMBRACE_METRIC', coreVitalParser(clsEntries));
-
-        if (window.webkit) {
-          window.webkit.messageHandlers.embrace.postMessage(coreVitalParser(clsEntries));
-        }
+        sendMessageToEmbrace(clsEntries);
       }
     }
-  }).observe({ type: 'layout-shift', buffered: true });
+  }).observe({ type: "layout-shift", buffered: true });
 }
